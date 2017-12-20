bb0d3466cd34d5729d4df8582afb4017090d3141
LaraBrockdorff <34508536+LaraBrockdorff@users.noreply.github.com>
2017-12-20 22:30:16 +0100

    Update main.c
    changing data type in decleration from float to int

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-float subtract(float num1,float num2);
+int subtract(float num1,float num2);
 float add(float num1,float num2);
 float multiplication(float num1, float num2);
 float division(float num1, float num2);
