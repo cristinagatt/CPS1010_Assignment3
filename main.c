dc4d902e5fd4dae047b92803a95a601f80a57aac
LaraBrockdorff <34508536+LaraBrockdorff@users.noreply.github.com>
2017-12-20 22:23:56 +0100

    Update main.c
    puttting working code

--- main.c
+++ main.c
@@ -1,8 +1,79 @@
-#include <stdio.l>
+#include <stdio.h>
 float subtract(float num1,float num2);
 float add(float num1,float num2);
 float multiplication(float num1, float num2);
 float division(float num1, float num2);
-
-
-int main(){
+ 
+ 
+int main() {
+ 
+    float num1;
+    float num2;
+    char menu;
+ 
+ 
+    printf("Enter 'a' for Addition\n");
+    printf("Enter 's' for Subtraction\n");
+    printf("Enter 'm' for Multiplication\n");
+    printf("Enter 'd' for Division\n");
+ 
+    scanf( "%c", &menu );
+ 
+    printf("enter num1 : ");
+    scanf("%f",&num1);
+ 
+    printf("enter num2 : ");
+    scanf("%f",&num2);
+ 
+ 
+ 
+ 
+    switch(menu){
+        case 'a': add(num1,num2);
+            break;
+        case 's': subtract(num1,num2);
+            break;
+        case 'm' :multiplication(num1,num2);
+            break;
+        case 'd':division(num1,num2);
+            break;
+    }
+ 
+ 
+ 
+    return  0;
+ 
+}
+ 
+float add(float num1, float num2){
+ 
+    float addition = num1 + num2;
+    printf("Addition is %.2f ",addition);
+    return addition;
+ 
+}
+ 
+float subtract(float num1,float num2){
+ 
+    float subtraction = num1 - num2;
+    printf("Subtraction is %.2f ",subtraction);
+    return subtraction;
+ 
+}
+ 
+ 
+float multiplication(float num1, float num2){
+ 
+    float multiplication = num1*num2;
+    printf("Multiplication is %.2f ",multiplication);
+    return multiplication;
+ 
+}
+ 
+float division(float num1, float num2){
+ 
+    float division = num1/num2;
+    printf("Division is %.2f ",division);
+    return division;
+ 
+}
